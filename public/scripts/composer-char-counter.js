c182d27f8cc0fcae0bb21db09357b73613071663
Rheaazarraga <rheaazarraga@gmail.com>
2021-09-14 04:19:43 +0000

    Apply single quotes for all strings within document for uniform formatting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,30 +1,30 @@
 $(document).ready(function () {
-  $("#tweet-box").on("input", function (event) {
+  $('#tweet-box').on('input', function (event) {
     //storing our form input
     let $formInput = $(this);
 
-    //targeting closest element to the form
+    //targets closest element to the form
     let $forms = $formInput.closest("form");
 
     //targets the text-box input
-    let $textCount = $forms.find("#tweet-box");
-    console.log("textCount", $textCount);
+    let $textCount = $forms.find('#tweet-box');
+    console.log('textCount', $textCount);
 
     //captures the length of our tweet
     let textLength = $textCount.val().length;
     let charLeft = 140 - textLength;
 
     //caputres the value of counter
-    let counterColor = $("#counter").val(charLeft);
-    console.log("counter", counterColor);
+    let counterColor = $('#counter').val(charLeft);
+    console.log('counter', counterColor);
 
     //if length of tweet input is >140 characters, 0 characters and within range
     //want to display or hide error msgs based on these scenarios
 
     if (charLeft < 0) {
-      counterColor.css("color", "red");
+      counterColor.css('color', 'red');
     } else {
-      counterColor.css("color", "white");
+      counterColor.css('color', 'white');
     }
   });
 });
