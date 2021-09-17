14cbb95bd1acbfd702bad177476b2f005a37a76b
Rheaazarraga <rheaazarraga@gmail.com>
2021-09-17 01:57:28 +0000

    Load hard coded array of users objects in to doc

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars:
+          "https://img.icons8.com/fluency/30/000000/lumpy-space-princess.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://img.icons8.com/color/30/000000/clr-beemo.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
+
+
