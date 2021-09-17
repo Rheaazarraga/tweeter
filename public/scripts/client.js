924585bef7af6ca1932be49cf77b6b196bdd0060
Rheaazarraga <rheaazarraga@gmail.com>
2021-09-17 02:01:53 +0000

    Apply logic for createTweetElement and rendering tweets. Insert timeago format to display timestamp of tweets on page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -30,5 +30,45 @@ $(document).ready(function () {
       created_at: 1461113959088,
     },
   ];
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
 
+    for (const tweet of tweets) {
+      const tweetMsg = createTweetElement(tweet);
+      $(".existing-tweets-container").append(tweetMsg);
+    }
+  };
+
+  const createTweetElement = function (tweet) {
+    const timeStamp = timeago.format(tweet.created_at);
+    let $tweet = `<article class="existing-tweets">
+
+  <header class="existing-tweets-header">
+    <div class="tweeter-avatar"> <img src="${tweet.user.avatars}"/> </div>
+    <div class="user-name"><b>${tweet.user.name}</b></div>
+    <div class="user-handle"><b>${tweet.user.handle}</b></div>
+    </header>
+
+
+  <div class="existing-tweets-body"> <strong>${tweet.content.text}</strong></div>
+
+
+      <footer class="existing-tweets-footer"><b>${timeStamp}</b>
+        <div class="tweet-icons">
+        <i id="icon1" class="fas fa-flag"></i>
+        <i id="icon2" class="fas fa-retweet"></i>
+        <i id="icon3" class="fas fa-heart"></i>
+      </div>
+    </footer>
+
+
+  </article>`;
+
+    return $tweet;
+  };
+
+  renderTweets(data);
+});
 
