b2ae338f1ba665e7bdb724eec73beaa038b88e3c
Rheaazarraga <rheaazarraga@gmail.com>
2021-09-17 03:23:16 +0000

    Implement logic for post jquery request for tweets

--- public/scripts/form-submissions.js
+++ public/scripts/form-submissions.js
@@ -1,2 +1,15 @@
+$(document).ready(function () {
 
+  $('#submit-form').on('submit', function (event) {
+    event.preventDefault();
+    const data = $( this ).serialize();
+    $.ajax({url: '/tweets', method: 'POST', data: data })
+    .then(function (response) {
+      console.log(`Response: ----`, response)
+      // comment to show its functioning
+    });
+  });
+
+
+});
 
