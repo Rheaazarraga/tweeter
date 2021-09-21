7cff0e383f8a0ec29aa8e569c663a59e8ba33889
Rheaazarraga <rheaazarraga@gmail.com>
2021-09-21 07:06:52 +0000

    Ensure unnecessary/extra tags/ code/ comments are removed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,4 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Leverage the createTweetElement function by passing the tweet object to it, then using the returned jQuery object by appending it to the #tweets-container section
 
 $(document).ready(function() {
   const renderTweets = function (tweets) {
@@ -11,13 +7,14 @@ $(document).ready(function() {
       $('.existing-tweets-container').prepend(tweetMsg);
     }
   };
-
+  // Secure input handling - re-encodes user text so that unsafe characters are converted into a safe "encoded" representation
   const escape = function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
+  // Rendering dynamic tweets dynamically from JS code and emptying out the existing-tweets-container from html
   const createTweetElement = function(tweet) {
     const timeStamp = timeago.format(tweet.created_at);
     let $tweet = `<article class="existing-tweets">
@@ -48,47 +45,60 @@ $(document).ready(function() {
   };
 
   const loadTweets = function() {
+    // Ajax, looking at url: '/tweets with get method
     $.ajax({ url: '/tweets', method: 'GET' })
+    // Result = results, passing into renderTweets(result)
       .then(result => renderTweets(result))
+       // If error, console.log error
       .catch(error => console.log(`Error:`, error));
   };
+
   let error = false;
+  // Handler to take care of Submit (Tweet) button functionality
   const submitHandler = function(event) {
+    // Prevents default behaviour
     event.preventDefault();
     let tweetBox = $('#tweet-box').val();
     const data = $(this).serialize();
 
+    // Function to take .JSON data from /tweets to pass
     const tweetPost = function(data) {
-      $.ajax({ url: '/tweets', method: 'POST', data: data }).then(() => {
-        $('.existing-tweets-container').empty();
-        $('#tweet-box').val('');
-        $('.alert').empty();
+      // Set #tweet-box to empty on successful submission & set .existing-tweets to empty to prevent duplicate messages
+       $.ajax({ url: '/tweets', method: 'POST', data: data }).then(() => {
+         $('.existing-tweets-container').empty();
+         $('#tweet-box').val('');
+         // Remove .alert if it existed
+         $('.alert').empty();
+         // Set #counter value back up to 140
         $('#counter').first().val(140);
         loadTweets();
       });
     };
-
+    // Function to handle error messages
     const errorHandler = function() {
+      // If tweet is empty
       if (tweetBox.length === 0) {
         $('.alert')
           .empty()
-          .append('<p>Error: Your tweet needs to be at least 1 character!</p>');
+          .append('<p>Brevity is the soul of wit, but your tweet does need at least 1 character long...</p>');
         $('.alert').hide().slideDown('slow');
         error = true
+        // If tweet exceeds max characters
       } else if (tweetBox.length > 140) {
         $('.alert')
           .empty()
-          .append('<p>Error: You\'ve reached the max amount of characters!</p>');
+          .append('<p>Looks like you\'ve reached the max amount of characters!</p>');
         $('.alert').hide().slideDown('slow');
         error = true;
       } else {
+        // Post tweet because no error exists
         tweetPost(data);
         error = false;
       }
     };
     errorHandler();
   };
-
+ // #text-box event listener to remove alert/error msg when typing
   $('form').on('submit', submitHandler),
   $('#tweet-box').on('keyup', () => {
     if (error === true) {
@@ -97,7 +107,7 @@ $(document).ready(function() {
    };
   });
 
-  //when clicking create tweet, new-tweet section will slide down for user to create a tweet. Click the button again, new-tweet section will slide up
+  // Upon clicking create a new tweet, the new-tweet section will slide down for user to create a tweet. Click the button again & new-tweet section will slide up and be re-hidden
   $('#create-tweet').on('click', () => {
     if($('#new-tweet').hasClass('hide-tweet')) {
       $('#new-tweet').slideDown('slow', function() {
