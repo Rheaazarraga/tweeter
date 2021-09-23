87dbe679883d13a111e47a22b228c1f45e5af8bf
Rheaazarraga <rheaazarraga@gmail.com>
2021-09-23 19:56:12 +0000

    Move scroll-btn functionality up within doc.ready and wrap scroll functions in function expressions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -99,6 +99,31 @@ $(document).ready(function() {
     };
     errorHandler();
   };
+    //Get the button:
+    scrollButton= document.getElementById("scroll-btn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+    
+    const scrollFunction = function() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.display = "block";
+      } else {
+        scrollButton.style.display = "none";
+      }
+    }
+    
+    // When the user clicks on the button, scroll to the top of the document
+    const topFunction = function() {
+      // For Safari
+      document.body.scrollTop = 0;
+      // For Chrome, Firefox, IE and Opera
+      document.documentElement.scrollTop = 0; 
+    }
+    $('#scroll-btn').on('click', () => {
+      topFunction();
+    })
+    
  // #text-box event listener to remove alert/error msg when typing
   $('form').on('submit', submitHandler),
   $('#tweet-box').on('keyup', () => {
@@ -120,29 +145,5 @@ $(document).ready(function() {
       });
     };
   });
-
   loadTweets();
 });
-
-  //Get the button:
-  scrollButton= document.getElementById("scroll-btn");
-
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function() {scrollFunction()};
-  
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      scrollButton.style.display = "block";
-    } else {
-      scrollButton.style.display = "none";
-    }
-  }
-  
-  // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    // For Safari
-    document.body.scrollTop = 0;
-    // For Chrome, Firefox, IE and Opera
-    document.documentElement.scrollTop = 0; 
-  }
-
