b37ded155df241c8782fdafff078ffbcd0603aaa
Rheaazarraga <rheaazarraga@gmail.com>
2021-09-23 19:35:30 +0000

    Add scroll to top button functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -123,3 +123,26 @@ $(document).ready(function() {
 
   loadTweets();
 });
+
+  //Get the button:
+  scrollButton= document.getElementById("scroll-btn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollButton.style.display = "block";
+    } else {
+      scrollButton.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    // For Safari
+    document.body.scrollTop = 0;
+    // For Chrome, Firefox, IE and Opera
+    document.documentElement.scrollTop = 0; 
+  }
+
